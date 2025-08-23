56f70fc6e0c671656f38d2b9c6e848fae020304a
kumari06khushi <khushi16bce@gmail.com>
2025-08-23 06:51:23 +0000

    add ones compliment and display

--- 02_oops.cpp
+++ 02_oops.cpp
@@ -1,29 +1,3 @@
-#include<iostream>
-using namespace std;
-
-class binary
-{
-    string s;
-
-    public:
-    void read(void);
-    void chk_bin(void);
-};
-void binary :: read(void){
-    cout<<"enter a binary number"<<endl;
-    cin>>s;
-}
-void binary ::chk_bin(void){
-    for (int i = 0; i <s.length(); i++)
-    {
-       if(s.at(i)!='0'  && s.at(i)!='1'){
-        cout<<"Incorrect binary formate"<<endl;
-        exit(0);
-       }
-    }
-    
-}
-int main(){
 //  oops-classes and objects
 // c++ --> initially called --> c with class by stroustroup
 // class --> extension of structure (in c)
@@ -41,8 +15,62 @@ int main(){
 //   khushi.salary= 8 make no sense if salary is private.
 
 // Nexting of member functions
+
+
+#include<iostream>
+using namespace std;
+
+class binary
+{
+    string s;
+
+    public:
+    void read(void);
+    void chk_bin(void);
+    void ones_compliment(void);
+    void display(void);
+};
+void binary :: read(void){
+    cout<<"enter a binary number"<<endl;
+    cin>>s;
+}
+void binary ::chk_bin(void){
+    for (int i = 0; i <s.length(); i++)
+    {
+       if(s.at(i)!='0'  && s.at(i)!='1'){
+        cout<<"Incorrect binary formate"<<endl;
+        exit(0);
+       }
+    }
+    
+}
+void binary :: ones_compliment(void){
+    for (int i = 0; i <s.length(); i++)
+    {
+       if(s.at(i)=='0'){
+       s.at(i)='1';
+       }
+    else{
+       s.at(i)='0';
+       }
+    }
+    
+}
+void binary ::display(void){
+    cout<<"Displaying your binary number is =";
+    for (int i = 0; i <s.length(); i++)
+    {
+      cout<< s.at(i);
+      
+    }
+    
+}
+
+int main(){
 binary b;
 b.read();
 b.chk_bin();
+b.ones_compliment();
+b.display();
     return 0;
 }
