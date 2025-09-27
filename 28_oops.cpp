37213a4f69fe7ac7fa232ee2c0811e4dd2a3f9bd
kumari06khushi <khushi16bce@gmail.com>
2025-09-27 09:18:36 +0530

    update intro pointer

--- 28_oops.cpp
+++ 28_oops.cpp
@@ -7,9 +7,10 @@ int main(){
 
     //new keyword dynamic allocation
     //int *ptr=new int(40);
-     float *ptr =new  float(40.56);
+     float *ptr =new float(40.56); //new float(40.56) → creates one float and initializes it with 40.56.
+                                     //new float[3] → creates an array of 3 floats, uninitialized (garbage values until you assign).
      cout<<"the value of at  adress p is"<<*(ptr)<<endl;
-     int *arr= new int[3];
+     int *arr= new int[3];//This creates 3 continuous boxes in memory, each able to store an int.
      arr[0]=10;
       *(arr+1)=20;
        arr[2]=30;
